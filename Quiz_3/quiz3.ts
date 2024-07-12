fe179c16356373437a037fdc72524c714114f1c4
thidtanai <thidtanai_k@cmu.ac.th>
2024-07-12 22:22:14 +0700

    Refactor(quiz3): break down swap function and make output according to example

--- Quiz_3/quiz3.ts
+++ Quiz_3/quiz3.ts
@@ -1,32 +1,36 @@
 class ReverseEncoder {
-  encode(text: string): string {
+  private swapCharacters(word: string): string {
+    const swappedChars = word.split("").map((char) => {
+      let currChar = char.charCodeAt(0) - "a".charCodeAt(0);
+      let convertedChar = ((25 - currChar) % 26) + "a".charCodeAt(0);
+      return String.fromCharCode(convertedChar);
+    });
+    return swappedChars.join("");
+  }
+
+  public encode(text: string): void {
     // prepare text
     text = text.toLowerCase();
     const words = text.split(" ");
 
-    // encode words
-    const encodeWords = words.map((word) => {
-      let result: Array<string> = [];
-      for (let i = 0; i < word.length; i++) {
-        let currChar = word.charCodeAt(i) - "a".charCodeAt(0);
-        let convertChar = ((25 - currChar) % 26) + "a".charCodeAt(0);
-        result.push(String.fromCharCode(convertChar));
-      }
-      return result.join("");
-    });
+    const swappedWords = words.map((word) => this.swapCharacters(word));
+    const encodedText = swappedWords.reverse().join(" ");
 
-    // swap order
-    return encodeWords.reverse().join(" ");
+    console.log(encodedText);
   }
 
-  decode(text: string): string {
-    // use encode to decode
-    let result = this.encode(text);
+  public decode(text: string): void {
+    // prepare text
+    const words = text.split(" ");
+
+    const swappedWords = words.map((word) => this.swapCharacters(word));
+    let decodedText = swappedWords.reverse().join(" ");
+
     // change the first character to upper case
-    result = result[0].toUpperCase() + result.slice(1);
-    return result;
+    decodedText = decodedText[0].toUpperCase() + decodedText.slice(1);
+
+    console.log(decodedText);
   }
 }
 
 export default ReverseEncoder;
-
--- Quiz_3/test3.ts
+++ Quiz_3/test3.ts
@@ -1,5 +1,5 @@
 import ReverseEncoder from "./quiz3";
 
 const reverseEncoder = new ReverseEncoder();
-console.log(reverseEncoder.encode("Hello world")); //dliow svool
-console.log(reverseEncoder.decode("dliow svool")); //Hello world
+reverseEncoder.encode("Hello world"); //dliow svool
+reverseEncoder.decode("dliow svool"); //Hello world
