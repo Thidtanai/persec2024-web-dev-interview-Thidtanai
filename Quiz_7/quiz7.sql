a7f2fd0b9b3eab1343181bb396bc08d09f24309c
thidtanai <thidtanai_k@cmu.ac.th>
2024-07-13 21:35:45 +0700

    Feat: quiz7

---
+++ Quiz_7/quiz7.sql
@@ -0,0 +1,15 @@
+SELECT 
+    YEAR(o.order_date) AS sales_year,
+    SUM(p.unit_price * od.quantity * (1 - od.discount)) AS total_sales
+FROM 
+    orders o
+JOIN 
+    order_details od ON o.order_id = od.order_id
+JOIN 
+    products p ON od.product_id = p.product_id
+WHERE 
+    o.ship_region = 'Western Europe'
+GROUP BY 
+    YEAR(o.order_date)
+ORDER BY 
+    sales_year;
